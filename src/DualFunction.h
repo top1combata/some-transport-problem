3e9c0f540aad7b3f7db503beb22df87bc9c89cc9
///////// <savvamihalich@gmail.com>
2024-11-04 20:09:52 +0300

    add DualFunction class for handling Auto differentiation

---
+++ src/DualFunction.h
@@ -0,0 +1,150 @@
+#include <functional>
+#include <concepts>
+
+
+template<class Real>
+class DualFunction
+{
+public:
+
+    using FunctionType = std::function<Real(Real)>;
+
+    DualFunction(FunctionType _function, FunctionType _derivative) :
+        m_function(_function),
+        m_derivative(_derivative) 
+        {}
+
+    template<class Scalar>
+    explicit DualFunction(Scalar scalar) 
+    requires requires (Scalar scalar){static_cast<Real>(scalar);}
+        :
+        m_function([scalar](Real){return Real(scalar);}),
+        m_derivative([](Real){return Real(0);})
+        {}
+
+    const FunctionType& function() const
+    {
+        return m_function;
+    }
+
+    const FunctionType& derivative() const
+    {
+        return m_derivative;
+    }
+
+
+    DualFunction operator()(const DualFunction& g) const
+    {
+        auto function = [f = this->function(), g = g.function()](Real x)
+        {
+            return f(g(x));
+        };
+        auto derivative = [df = this->derivative(), g = g.function(), dg = g.derivative()](Real x)
+        {
+            return df(g(x)) * dg(x);
+        };
+        return DualFunction(function, derivative);
+    };
+
+private:
+
+    FunctionType m_function;
+    FunctionType m_derivative;
+};
+
+
+template<class Real>
+DualFunction<Real> operator+(const DualFunction<Real>& f, const DualFunction<Real>& g)
+{
+    auto function = [f = f.function(), g = g.function()](Real x)
+    {
+        return f(x) + g(x);
+    };
+
+    auto derivative = [df = f.derivative(), dg = g.derivative()](Real x)
+    {
+        return df(x) + dg(x);
+    };
+    return DualFunction<Real>(function, derivative);
+}
+
+
+template<class Real>
+DualFunction<Real> operator-(const DualFunction<Real>& f, const DualFunction<Real>& g)
+{
+    auto function = [f = f.function(), g = g.function()](Real x)
+    {
+        return f(x) - g(x);
+    };
+
+    auto derivative = [df = f.derivative(), dg = g.derivative()](Real x)
+    {
+        return df(x) - dg(x);
+    };
+    return DualFunction<Real>(function, derivative);
+}
+
+
+template<class Real>
+DualFunction<Real> operator*(const DualFunction<Real>& f, const DualFunction<Real>& g)
+{
+    auto function = [f = f.function(), g = g.function()](Real x)
+    {
+        return f(x) * g(x);
+    };
+
+    auto derivative = [f = f.function(), g = g.function(), df = f.derivative(), dg = g.derivative()](Real x)
+    {
+        return f(x) * dg(x) + df(x) * g(x);
+    };
+    return DualFunction<Real>(function, derivative);
+}
+
+
+template<class Real>
+DualFunction<Real> operator/(const DualFunction<Real>& f, const DualFunction<Real>& g)
+{
+    auto function = [f = f.function(), g = g.function()](Real x)
+    {
+        return f(x) / g(x);
+    };
+
+    auto derivative = [f = f.function(), g = g.function(), df = f.derivative(), dg = g.derivative()](Real x)
+    {
+        Real gx = g(x);
+        return (df(x) * gx - f(x) * dg(x)) / gx / gx;
+    };
+    return DualFunction<Real>(function, derivative);
+}
+
+
+template<class Real, class Scalar>
+requires requires (Scalar scalar){static_cast<Real>(scalar);}
+DualFunction<Real> operator*(const DualFunction<Real>& f, Scalar scalar)
+{
+    auto function = [f = f.function(), scalar](Real x)
+    {
+        return scalar * f(x);
+    };
+
+    auto derivative = [df = f.derivative(), scalar](Real x)
+    {
+        return scalar * df(x);
+    };
+    return DualFunction<Real>(function, derivative);
+}
+
+
+template<class Real, class Scalar>
+DualFunction<Real> operator*(Scalar scalar, const DualFunction<Real>& f)
+requires requires (Scalar scalar){static_cast<Real>(scalar);}
+{
+    return f * scalar;
+}
+
+
+template<class Real>
+DualFunction<Real> operator-(const DualFunction<Real>& f)
+{
+    return (-1) * f;
+}
