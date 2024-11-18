c59b9db7de21976afed03eb83585aeca05e21014
///////// <savvamihalich@gmail.com>
2024-11-19 00:50:54 +0300

    add conjugate gradient solver with golden ratio search for non linear problems

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -10,6 +10,7 @@ using Vector = Eigen::Matrix<double, Eigen::Dynamic, 1>;
 
 DualFunction<double> getY(const Vector& coeffs);
 Vector gradient(const Vector& coeffs, double);
+double goldenRatioSearch(const std::function<double(double)>& f, double l, double r, double eps);
 
 
 DualFunction<double> RayleighRitz(unsigned n, double eps, double step)
@@ -19,20 +20,89 @@ DualFunction<double> RayleighRitz(unsigned n, double eps, double step)
     assert(n != 0);
 
     Vector coeffs = Vector::Zero(n, 1);
+    Vector prevGrad = gradient(coeffs, step * eps);
+    Vector grad  = prevGrad;
+    Vector conjugate = -prevGrad;
+
+    std::function<double(double)> f = [&coeffs, &conjugate](double x)
+    {
+        try
+        {
+            return ProblemFunctional(getY(coeffs + x * conjugate));
+        }
+        catch (const char* message)
+        {
+            std::cout << "ERROR : " << message << '\n';
+            std::exit(0);
+        }
+    };
 
     double error = std::numeric_limits<double>::max();
-    Vector velocity = Vector::Zero(n, 1);
-    constexpr double beta = 0.9;
 
     for (int iterNum = 0; error > eps; iterNum++)
     {
-        velocity = beta * velocity + step * gradient(coeffs - beta * velocity, eps);
-        error = velocity.norm();
-        coeffs -= velocity;
-        std::cout << "Error : " << std::scientific << error << '\n';
+        prevGrad = grad;
+        grad = gradient(coeffs, step * eps);
+
+        // Polak–Ribiere
+        double beta = (iterNum ? grad.dot(grad - prevGrad) / prevGrad.dot(prevGrad) : 0);
+        beta = std::max(beta, 0.0);
+
+        conjugate = -grad + beta * conjugate;
+        double alpha = goldenRatioSearch(f, 0, step, eps * step);
+
+        coeffs += alpha * conjugate;
+        // error = (coeffs - prevCoeffs).norm();
+        error = grad.norm();
+        std::cout << "Residual " << std::scientific << error << '\n';
     }
 
     return getY(coeffs);
+}
+
+
+double goldenRatioSearch(const std::function<double(double)>& f, double l, double r, double eps)
+{
+    using std::numbers::phi;
+
+    struct point
+    {
+        double x, y;
+    };
+
+    // left.x < middleLeft.x < middleRight.x < right.x
+    point left, right, middleLeft, middleRight;
+    left.x        = l;
+    right.x       = r;
+    middleLeft.x  = (r - l) / (phi*phi) + l;
+    middleRight.x = right.x - middleLeft.x + left.x;
+    
+    left.y        = f(left.x);
+    middleLeft.y  = f(middleLeft.x);
+    middleRight.y = f(middleRight.x);
+    right.y       = f(right.x);
+
+    while (right.x - left.x > eps)
+    {
+        if (middleLeft.y < middleRight.y)
+        {
+            right = middleRight;
+            middleRight = middleLeft;
+
+            middleLeft.x = left.x + right.x - middleRight.x;
+            middleLeft.y = f(middleLeft.x);
+        }
+        else
+        {
+            left = middleLeft;
+            middleLeft = middleRight;
+
+            middleRight.x = right.x - middleLeft.x + left.x;
+            middleRight.y = f(middleRight.x);
+        }
+    }
+    
+    return (left.x + right.x) / 2;
 }
 
 
